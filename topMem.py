778892963d912d8c345761964351e4bba2ed83f0
Redding <yinzhiyuan@orvibo.com>
2021-11-27 17:20:17 +0800

    显示三行一列图表，CPU使用情况，io使用情况，内存使用情况

--- topMem.py
+++ topMem.py
@@ -18,15 +18,19 @@ class DataDraw:
         pass
 
 
-fp = open("./top.txt", "r", errors='ignore', encoding='utf-8')
+fp = open("./mem.txt", "r", errors='ignore', encoding='utf-8')
 line = fp.readline()
 #data = set()
 #data = list()
 data = dict()
 cpu_data = dict()
+mem_data = dict()
 
 show_array = ['usr','sys','nic','idle','io','irq','sirq']
 hide_array = ['nic','idle','irq','sirq','usr','sys']
+#proc_array = ["vispeech","/oem/app/flutter-gui/gui_program/mixpad_gui","vicenter","audio_manager"]
+proc_array = ["vispeech","vifamily","/oem/app/flutter-gui/gui_program/mixpad_gui","system_manager","vicenter","audio_manager","guiservice","mixpad_music","/oem/ember-host/bin/ember-host"]
+proc_mem_names = ["VmRSS", "VmData"]
 #['usr','sys','nic','idle','io','irq','sirq']
 #['used','free','shrd','buff','cached']
 
@@ -70,17 +74,35 @@ while line is not None:
                 cpu_data[arr2[1]] = list()
             num = int(arr2[0].strip('%'))
             cpu_data[arr2[1]].append(num)
+    else:
+        for name in proc_array:
+            if line.startswith(name):
+                if name not in mem_data:
+                    mem_data[name] = dict()
+                arr0 = line.split(':')
+                arr1 = arr0[1].split(';')
+                for m in arr1:
+                    arr2 = m.split('=')
+                    if arr2[0] not in mem_data[name]:
+                        mem_data[name][arr2[0]] = list()
+                    num = int(arr2[1].strip('\n'))
+                    mem_data[name][arr2[0]].append(num)
+
+
     line = fp.readline()
     if not line:
         print("not read content")
         break
 
+#print("format data: ", mem_data)
 fig = plt.figure()
-a1 = fig.add_subplot(2,1,1)
-a2 = fig.add_subplot(2,1,2)
+a1 = fig.add_subplot(3,1,1)
+a2 = fig.add_subplot(3,1,2)
+a3 = fig.add_subplot(3,1,3)
 for key in data:
-    if key not in hide_array:
-        a1.plot(data[key], label = key)
+    if key in hide_array:
+        continue
+    a1.plot(data[key], label = key)
     """
     num = max(data[key])
     if(num<80000):
@@ -95,8 +117,21 @@ for key in cpu_data:
     if key not in hide_array:
         a2.plot(cpu_data[key], label = key)
 
+color_array = ["aliceblue", "black", "blue", "brown", "coral", "tomato", "pink", "yellow", "green", "red"]
+i = 0
+for key in mem_data:
+    i = i+1
+    #vm = "VmRSS" 
+    vm = "VmData"
+    a3.plot(mem_data[key][vm], label = key+"." + vm, color = color_array[i])
+    """
+    for k in mem_data[key]:
+        a3.plot(mem_data[key][k], label = key+"."+k, color = color_array[i])
+    """
+
 y_major_locator=MultipleLocator(5000)
 a1.yaxis.set_major_locator(y_major_locator)
 a1.legend()
 a2.legend()
+a3.legend()
 plt.show()
