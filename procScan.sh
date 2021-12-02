c96c44f482b3cf0ae25a41954de3dbd09383d0e3
Redding <yinzhiyuan@orvibo.com>
2021-12-02 10:21:38 +0800

    优化awk过滤条件，解决进程不存在时数据不正确的问题

--- procScan.sh
+++ procScan.sh
@@ -3,17 +3,18 @@
 filePath="/tmp/mem.txt"
 
 scanProcMemory(){
-	for proc in "vispeech" "vifamily" "/oem/app/flutter-gui/gui_program/mixpad_gui" "system_manager" "vicenter" "audio_manager" "guiservice" "mixpad_music" "/oem/ember-host/bin/ember-host" "dbus-daemon"
+	for proc in "vispeech" "vifamily" "\/oem\/app\/flutter-gui\/gui_program\/mixpad_gui" "system_manager" "vicenter" "audio_manager" "guiservice" "mixpad_music" "\/oem\/ember-host\/bin\/ember-host" "dbus-daemon"
 	do
-		pid=`ps -aux | awk '$5==name {printf "%s ",$1}' name="$proc"`
+		#ps:获取所有进程信息；grep:过滤掉影响下一条命令执行的内容；awk:提取目标数据
+		pid=`ps -aux | grep -v "awk" | awk '/.*'$proc'.*/ {printf "%s ",$1}'`
 		set -- $pid
 		#there can be serval pids for a proc
 		for item in $pid
 		do
 			mem=`cat /proc/$item/status | awk '{ if($1=="VmRSS:") printf "VmRSS=%s;",$2;else if($1=="VmData:") printf "VmData=%s",$2;}'`
+			echo "$proc:$mem" >> $filePath
 		done
 		#cat /proc/$pid/status | awk '$1=="VmRSS:" {print "1:",$1,"2:",$2}'
-		echo "$proc:$mem" >> $filePath
 	done
 }
 
