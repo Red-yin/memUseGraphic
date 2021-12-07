07891b9003d724b6f2301e8fa3073fe3aa9c2d88
Redding <yinzhiyuan@orvibo.com>
2021-12-07 16:08:45 +0800

    更新README.md；优化topMem.py代码流程，兼容orb.log日志内容图表化

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 procScan.sh: 周期性地检测CPU使用情况和几个主要进程的内存使用情况，结果保存到/tmp/mem.txt文件；
 topMem.py: 将mem.txt文件数据格式化，以点线图的方式画出表格；
+结果获取(linux环境)：导出/tmp/mem.txt文件，与topMem.py放到同一目录下，运行topMem.py
+
 
 执行：
-./procScan.sh 数字
+./procScan.sh 数字 &
 可以设置CPU检测和进程内存检测的时间周期，以秒为单位，例如：./procScan.sh 3，不带参数时默认时间周期为1秒
 
--- topMem.py
+++ topMem.py
@@ -1,177 +1,276 @@
 #!/usr/bin/python3
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import MultipleLocator
+from matplotlib.pyplot import LinearLocator
 from matplotlib import dates
 
 import os
 import sys
 import time
+import re
+
+def VmRSS_VmData_cbk(line):
+    ret_d = dict()
+    arr0 = line.split(':')
+    if len(arr0) < 2:
+        print(line)
+        return ret_d
+    name = arr0[0]
+    ret_d[name] = dict()
+    arr1 = arr0[1].split(';')
+    s = 0
+    for m in arr1:
+        arr2 = m.split('=')
+        if len(arr2) < 2:
+            continue
+        num = int(arr2[1].strip('\n'))
+        ret_d[name][arr2[0]] = num
+
+    return ret_d
+
+def datestamp_cbk(line):
+    ret_d = dict()
+          #日期记录数据
+    arr0 = line.split(': ')
+    if len(arr0) < 2:
+        print(line)
+        return ret_d
+    arr1 = arr0[1].split()
+    if len(arr1) < 4:
+        print(line)
+        return ret_d
+    ret_d['datestamp'] = arr1[3]
+ 
+    return ret_d
+
+def timestamp_cbk(line):
+    ret_d = dict()
+        #时间戳记录数据
+    arr0 = line.split(':')
+    if len(arr0) < 2:
+        print(line)
+        return ret_d
+    ret_d['timestamp'] = arr0[1]
+   
+    return ret_d
+
+def CPU_cbk(line):
+    ret_d = dict()
+    arr0 = line.split(':')
+        #for c in arr0[1]:
+            #print(hex(ord(c)))
+    arr1 = arr0[1].split('  ')
+    for item in arr1:
+        arr2 = item.split()
+        if len(arr2) < 2:
+            #print(line)
+            continue
+        num = int(arr2[0].strip('%'))
+        ret_d[arr2[1]] = num
+ 
+    return ret_d
+
+def Mem_cbk(line):
+    ret_d = dict()
+    arr0 = line.split(':')
+    arr1 = arr0[1].split(',')
+    s = 0
+    free = 0
+    used = 0
+    for item in arr1:
+        arr2 = item.split(  )
+        num = int(arr2[0].strip('K'))
+        if(arr2[1] == 'used'):
+            used = num
+        elif(arr2[1] == 'free'):
+            free = num
+            continue
+        ret_d[arr2[1]] = num
+    total = free + used
+    ret_d['total'] = total
+    return ret_d
+
+def orb_log_cbk(line):
+    ret_d = dict()
+    arr0 = line.split()
+    if len(arr0) < 2:
+        print(line)
+        return ret_d
+    cpu_array = arr0[4].split(':')
+    rss_array = arr0[5].split(':')
+    time_array = arr0[1].split('][')
+    if len(cpu_array) < 2 or len(rss_array) < 2 or len(time_array) < 2:
+        print(line)
+        return ret_d
+
+    ret_d['time'] = time_array[0]
+    ret_d['cpu'] = cpu_array[1].strip('%')
+    ret_d['rss'] = int(rss_array[1].strip('K'))
+    return ret_d
 
 class DataFormat:
     #输入数据格式化
-    def __init__():
-        pass
+    def __init__(self, filename="./mem.txt"):
+        self.file_path = filename
+        if not os.path.exists(self.file_path):
+            print(self.file_path + "is not exist")
+            exit(0)
+
+    def _data_append(self, dst, src):
+        for key in src:
+            if type(src[key]) is not dict:
+                if key not in dst:
+                    dst[key] = list()
+                dst[key].append(src[key])
+                #print(key, " len is ", len(dst[key]))
+            else:
+                if key not in dst:
+                    dst[key] = dict()
+                self._data_append(dst[key], src[key])
+                    
+    def _file_read(self):
+        fp = open(self.file_path, "r", errors='ignore', encoding='utf-8')
+        line = fp.readline()
+        self.data = dict()
+        while line is not None:
+            ret = self.data_filter(line)
+            self._data_append(dst=self.data, src=ret)
+
+            line = fp.readline()
+            if not line:
+                print("file end")
+                for key in self.data:
+                    if key.find('mixpad_gui') != -1 or key.find('ember-host') != -1:
+                        print(key, self.data[key])
+                        print(len(self.data[key]))
+                    #print(key, len(self.data[key]))
+                break
+
+
+    def data_filter(self, line):
+        m_dict = {'.*\[monitor INFO\].*vispeech\[.*': orb_log_cbk,
+                'Mem:.*': Mem_cbk,
+                'CPU:.*': CPU_cbk,
+                'datestamp.*': datestamp_cbk,
+                'timestamp.*': timestamp_cbk,
+                '.*:VmRSS=.*;VmData.*': VmRSS_VmData_cbk}
+        ret_d = dict()
+        for key in m_dict:
+            matchLine = re.search(key, line)
+            if matchLine is not None:
+                ret_d = m_dict[key](line)
+        return ret_d
+
+    def run(self):
+        self._file_read()
+
 
 class DataDraw:
     #格式化数据可视化
-    def __init__():
-        pass
+    fig_type = {'time': 1, 'datestamp': 2} #根据数据中的关键词区分图形显示样式
+    cpu_keys = ['usr','sys','nic','idle','io','irq','sirq']
+    mem_keys = ['used','free','shrd','buff','cached']
+    proc_keys = ["vispeech","vifamily","mixpad_gui","system_manager","vicenter","audio_manager","guiservice","mixpad_music","ember-host","dbus-daemon"]
+    hide_keys = ['nic','idle','irq','sirq','usr','sys', 'VmData','\/oem\/ember-host\/bin\/ember-host']
+    def __init__(self, data):
+        self.data = data
+        self.type = self.figure_type_check(data)
+        self.fig = plt.figure()
+        if self.type == 1:
+            self.cpu_figure = self.fig.add_subplot(2, 1, 1)
+            self.rss_figure = self.fig.add_subplot(2, 1, 2)
+        elif self.type == 2:
+            self.cpu_figure = self.fig.add_subplot(3, 1, 2)
+            self.mem_figure = self.fig.add_subplot(3, 1, 1)
+            self.proc_figure = self.fig.add_subplot(3, 1, 3)
 
-print(sys.argv)
-file_path = "./mem.txt"
-if len(sys.argv) > 1:
-    file_path = sys.argv[1]
+    def list_align(self, l0, l1):
+        len0 = len(l0)
+        len1 = len(l1)
+        #print(len0,":", len1)
+        #print(l0, l1)
+        while len(l0) > len1:
+            l0.pop()
+        while len(l1) > len0:
+            l1.pop()
+ 
+    def figure_type_check(self, data):
+        for key in data:
+            if key in self.fig_type:
+                return self.fig_type[key]
 
-fp = open(file_path, "r", errors='ignore', encoding='utf-8')
-line = fp.readline()
-#data = set()
-#data = list()
-data = dict()
-cpu_data = dict()
-mem_data = dict()
-time_data = dict()
+    def run(self):
+        color_array = ["aliceblue", "black", "blue", "brown", "coral", "tomato", "pink", "yellow", "green", "red", "gray", "cyan", "darkgreen", "darkgray", "burlywood"]
+        
+        i_cpu = 0
+        i_mem = 0
+        i_proc = 0
+        for key in self.data:
+            print(key)
+            if key in self.hide_keys:
+                #隐藏的数据不显示线条
+                continue
 
-show_array = ['usr','sys','nic','idle','io','irq','sirq']
-hide_array = ['nic','idle','irq','sirq','usr','sys']
-#proc_array = ["vispeech","/oem/app/flutter-gui/gui_program/mixpad_gui","vicenter","audio_manager"]
-proc_array = ["vispeech","vifamily","mixpad_gui","system_manager","vicenter","audio_manager","guiservice","mixpad_music","ember-host","dbus-daemon"]
-proc_mem_names = ["VmRSS", "VmData"]
-#['usr','sys','nic','idle','io','irq','sirq']
-#['used','free','shrd','buff','cached']
-
-while line is not None:
-    if line.find('Mem:') != -1:
-        arr0 = line.split(':')
-        arr1 = arr0[1].split(',')
-        s = 0
-        free = 0
-        used = 0
-        for item in arr1:
-            arr2 = item.split(  )
-            num = int(arr2[0].strip('K'))
-            if(arr2[1] == 'used'):
-                used = num
-            elif(arr2[1] == 'free'):
-                free = num
+            if self.type == 1 and key == 'time':
                 continue
-            if arr2[1] not in data:
-                data[arr2[1]] = list()
-            data[arr2[1]].append(num)
-        total = free + used
-        if 'total' not in data:
-            data['total'] = list()
-        data['total'].append(total)
-    elif line.find('CPU:') != -1:
-        arr0 = line.split(':')
-        #for c in arr0[1]:
-            #print(hex(ord(c)))
-        arr1 = arr0[1].split('  ')
-        for item in arr1:
-            arr2 = item.split()
-            if len(arr2) < 2:
+            elif self.type == 2 and key == 'datestamp':
                 continue
-            if arr2[1] not in cpu_data:
-                cpu_data[arr2[1]] = list()
-            num = int(arr2[0].strip('%'))
-            cpu_data[arr2[1]].append(num)
-    elif line.find('stamp') != -1:
-        if line.startswith('datestamp'):
-            #日期记录数据
-            arr0 = line.split(': ')
-            if len(arr0) < 2:
-                continue
-            arr1 = arr0[1].split()
-            if len(arr1) < 4:
-                continue
-            if 'datestamp' not in time_data:
-                time_data['datestamp'] = list()
-            time_data['datestamp'].append(arr1[3])
-        elif line.startswith('timestamp'):
-            #时间戳记录数据
-            arr0 = line.split(':')
-            if len(arr0) < 2:
-                continue
-            if 'timestamp' not in time_data:
-                time_data['timestamp'] = list()
-            time_data['timestamp'].append(arr0[1])
-    else:
-        for name in proc_array:
-            if line.find(name) != -1:
-                if name not in mem_data:
-                    mem_data[name] = dict()
-                arr0 = line.split(':')
-                if len(arr0) < 2:
-                    continue
-                arr1 = arr0[1].split(';')
-                s = 0
-                for m in arr1:
-                    arr2 = m.split('=')
-                    if len(arr2) < 2:
+            if type(self.data[key]) is list:
+                if self.type == 1:
+                    i_cpu = i_cpu + 1
+                    self.list_align(l0=self.data['time'], l1=self.data[key])
+                    if key == 'cpu':
+                        self.cpu_figure.plot(self.data['time'], self.data[key], label = key, color = color_array[i_cpu])
+                    elif key == 'rss':
+                        self.rss_figure.plot(self.data['time'], self.data[key], label = key, color = color_array[i_cpu])
+                elif self.type == 2:
+                    if key in self.cpu_keys:
+                        i_cpu = i_cpu + 1
+                        self.cpu_figure.plot(self.data[key], label = key, color = color_array[i_cpu])
+                    elif key in self.mem_keys:
+                        i_mem = i_mem + 1
+                        self.mem_figure.plot(self.data[key], label = key, color = color_array[i_mem])
+                    elif key in self.proc_keys:
+                        i_proc = i_proc + 1
+                        self.proc_figure.plot(self.data[key], label = key, color = color_array[i_proc])
+            elif type(self.data[key]) is dict:
+                i_proc = i_proc + 1
+                for k in self.data[key]:
+                    if k in self.hide_keys:
+                        #隐藏的数据不显示线条
                         continue
-                    if arr2[0] not in mem_data[name]:
-                        mem_data[name][arr2[0]] = list()
-                    num = int(arr2[1].strip('\n'))
-                    mem_data[name][arr2[0]].append(num)
+                    self.list_align(l0=self.data['datestamp'], l1=self.data[key][k])
+                    self.proc_figure.plot(self.data['datestamp'], self.data[key][k], label = key + "." + k, color = color_array[i_proc])
+            #self.a1.plot(self.data['time'], self.data[key], label = key, color = color_array[i])
+
+        if self.type == 1:
+            self.cpu_figure.legend()
+            self.rss_figure.legend()
+            x_major_locator=LinearLocator(15)
+            y_major_locator=LinearLocator(10)
+            self.cpu_figure.yaxis.set_major_locator(y_major_locator)
+            self.rss_figure.xaxis.set_major_locator(x_major_locator)
+        elif self.type == 2:
+            self.proc_figure.legend()
+            self.cpu_figure.legend()
+            self.mem_figure.legend()
+            self.proc_figure.grid()
+            #x_major_locator=MultipleLocator(180)
+            #y_major_locator=MultipleLocator(5000)
+            x_major_locator=LinearLocator(20)
+            y_major_locator=LinearLocator(10)
+            self.proc_figure.yaxis.set_major_locator(y_major_locator)
+            self.proc_figure.xaxis.set_major_locator(x_major_locator)
+            #plt.ylim(100, 220000)
+
+        plt.show()
 
 
-
-    line = fp.readline()
-    if not line:
-        print("not read content")
-        break
-
-
-#print("format data: ", mem_data)
-fig = plt.figure()
-#a1 = fig.add_subplot(3,1,1)
-#a2 = fig.add_subplot(3,1,2)
-a3 = fig.add_subplot(1,1,1)
-for key in data:
-    if key in hide_array:
-        continue
-    #a1.plot(data[key], label = key)
-    """
-    num = max(data[key])
-    if(num<80000):
-        print(key + ":", max(data[key]))
-        a1.plot(data[key], label = key)
-    else:
-        print(key + ":", max(data[key]))
-        a2.plot(data[key], label = key)
-    """
-
-    """
-for key in cpu_data:
-    if key not in hide_array:
-        a2.plot(cpu_data[key], label = key)
-
-    """
-color_array = ["aliceblue", "black", "blue", "brown", "coral", "tomato", "pink", "yellow", "green", "red", "gray"]
-i = 0
-for key in mem_data:
-    i = i+1
-    vm = "VmRSS" 
-    #vm = "VmData"
-    l = len(mem_data[key][vm])
-    t_len = len(time_data['datestamp'])
-    while len(time_data['datestamp']) > l:
-        time_data['datestamp'].pop()
-    while len(mem_data[key][vm]) > t_len:
-        mem_data[key][vm].pop()
-    a3.plot(time_data['datestamp'], mem_data[key][vm], label = key+"." + vm, color = color_array[i])
-    """
-    for k in mem_data[key]:
-        a3.plot(mem_data[key][k], label = key+"."+k, color = color_array[i])
-    """
-
-x_major_locator=MultipleLocator(180)
-y_major_locator=MultipleLocator(1000)
-#a1.yaxis.set_major_locator(y_major_locator)
-#a1.legend()
-#a2.legend()
-a3.yaxis.set_major_locator(y_major_locator)
-a3.xaxis.set_major_locator(x_major_locator)
-a3.legend()
-plt.ylim(100, 220000)
-a3.grid()
-plt.show()
+if __name__=="__main__":
+    print("start............")
+    df = DataFormat(sys.argv[1])
+    df.run()
+    dd = DataDraw(df.data)
+    dd.run()
+    print("end............")
+    exit()
